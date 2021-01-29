135202e9d0515708c79a33b6c5b2f7d64610ef5a
David He <jialhe85@gmail.com>
2021-01-29 11:58:49 +1100

    Update publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,11 +5,7 @@ set -euo pipefail
 IMAGE_NAME="$npm_package_name"
 VERSION_TAG="$npm_package_version"
 
-echo "new ver tag... $IMAGE_NAME:$VERSION_TAG"
+echo "new tag... $IMAGE_NAME:$VERSION_TAG"
 
-# VERSION_TAG=$(cat ../../lerna.json | jq -r '.version')
+# your image publish logic
 
-# echo "Publishing ${IMAGE_NAME} with version ${VERSION_TAG} to ${DOCKER_REGISTRY}"
-
-# docker tag ${IMAGE_NAME} ${DOCKER_REGISTRY}/nab/${IMAGE_NAME}:${VERSION_TAG}
-# docker push ${DOCKER_REGISTRY}/nab/${IMAGE_NAME}:${VERSION_TAG} > /dev/null
