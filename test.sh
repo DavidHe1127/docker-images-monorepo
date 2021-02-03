89e8a852ef4b4168f8ff1a5d5c10abf5ab6a86fd
David He <jialhe85@gmail.com>
2021-02-04 07:06:51 +1100

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 git config user.email "jialhe85@gmail.com"
 git remote set-url origin "https://${GH_TOKEN}@github.com/DavidHe1127/docker-images-monorepo.git"
-git fetch --no-tags --progress -- origin +refs/heads/main:refs/remotes/origin/main
-git checkout main && git pull -r
+# git fetch --no-tags --progress -- origin +refs/heads/main:refs/remotes/origin/main
+# git checkout main && git pull -r
 
 yarn set-new-version:ci
