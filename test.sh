7b120ad5f6d929da519e9f6a71a6b84f3c543510
David He <jialhe85@gmail.com>
2021-02-04 10:41:13 +1100

    Update test again

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+git remote rm origin
+git remote add origin "https://${GH_TOKEN}@github.com/DavidHe1127/docker-images-monorepo.git"
+git fetch
 git config user.email "jialhe85@gmail.com"
-git remote set-url origin "https://${GH_TOKEN}@github.com/DavidHe1127/docker-images-monorepo.git"
+git config user.name "Lerna"
 # git fetch --no-tags --progress -- origin +refs/heads/main:refs/remotes/origin/main
 # git checkout main && git pull -r
 
